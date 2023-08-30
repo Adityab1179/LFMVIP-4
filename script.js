a996f696855507c74ad433376626794071c53ffd
Aditya Bansal <110539909+Adityab1179@users.noreply.github.com>
2023-08-30 08:32:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
